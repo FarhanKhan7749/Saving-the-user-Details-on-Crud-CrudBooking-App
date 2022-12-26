c2eb82dbc99bc29b39ee842b0498efcf256e640f
FarhanKhan7749 <119105808+FarhanKhan7749@users.noreply.github.com>
2022-12-26 10:14:36 -0800

    Delete User

--- main.js
+++ main.js
@@ -1,61 +1,51 @@
 var form = document.querySelector("#my-form");
-var nameInput = document.querySelector('#name');
-var emailInput = document.querySelector('#email');
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    // user object 
-    let user ={
-        name : nameInput.value,
-        email : emailInput.value,
-    } 
-    document.querySelector("#my-form").reset();// to reset the form
-    // create user in backed useing network call
-    axios.post('https://crudcrud.com/api/373328cd2d974b7d9c8c0727f5be42c5/appointmentData', user)
-    .then((result) => {
-        console.log(result);     
-    }).catch((err) => {
-        console.log(err);
-    });
-    addListOfUsers(user);// calling the funtion to display the created user
+var nameInput = document.querySelector("#name");
+var emailInput = document.querySelector("#email");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // user object
+  let user = {
+    name: nameInput.value,
+    email: emailInput.value,
+  };
+  document.querySelector("#my-form").reset(); // to reset the form
+  axios.post('https://crudcrud.com/api/ad8634a3680849ea9d4ceadd1bfa2b3b/appointmentData', user)
+  .then((result) => {
+      console.log(result);
+  }).catch((err) => {
+      console.log(err);
+  });
+  addListOfUsers(user); // calling the funtion to display the created user
 });
 // Create list in html document
 function addListOfUsers(user) {
-    // get element where you wnat to create a elemnt 
-    var ul = document.getElementById("listOfUser");
-    // once get the element then create a element 
-    var li = document.createElement("li");
-    // append the elemet in child, child is because the we are creating the element so it will child of the the element we want ot add the element.
-    li.appendChild(document.createTextNode(user.name + " " + user.email + " "));
-    // append child node in paprent element or the where you want to add the element.
-
-    //edit button
-    var editB = document.createElement('input');
-    editB.type = 'button'
-    editB.value = 'Edit'
-    editB.addEventListener('click',(e)=>{
-        document.getElementById('name').value = user.name;
-        document.getElementById('email').value = user.email;
-        li.remove();
-    })
-    editB.style.border = "2px solid green";
-    //delete button
-    var deleteB = document.createElement('input');
-    deleteB.type = 'button'
-    deleteB.value = 'Delete'
-    deleteB.addEventListener('click',(e) =>{
-        localStorage.removeItem(user.email);
-        li.remove();
-    })
-    deleteB.style.border = "2px solid red";
-    //
-    li.appendChild(editB);
-    li.appendChild(deleteB);
-    ul.append(li)
+  var userId = user._id;
+  console.log(userId);
+  const parentNode = document.getElementById("listOfUser");
+  const childHTML = `<li id=${user._id}> ${user.name} ${user.email} <button onclick=deleteUser('${user._id}')>delete</button> <button onclick=editUser('${user._id}')>Edit</button></li>`;
+  console.log(childHTML);
+  parentNode.innerHTML = parentNode.innerHTML + childHTML;
 }
-
+function deleteUser(userId) {
+  axios.delete(`https://crudcrud.com/api/ad8634a3680849ea9d4ceadd1bfa2b3b/appointmentData/${userId}`)
+  .then((res)=>{
+      console.log(res);
+      removeUserFromScreen(userId);
+  })
+  .catch((err)=>{
+      console.log(err);
+  })
+}
+function removeUserFromScreen(userId) {
+  const parentNode = document.getElementById("listOfUser");
+  const childNodeToBeDeleted = document.getElementById(userId);
+  if(childNodeToBeDeleted) {
+    parentNode.removeChild(childNodeToBeDeleted);
+  }
+}
 //get request using crud crud
 function showUser1(){
-    axios.get('https://crudcrud.com/api/373328cd2d974b7d9c8c0727f5be42c5/appointmentData')
+    axios.get('https://crudcrud.com/api/ad8634a3680849ea9d4ceadd1bfa2b3b/appointmentData')
     .then((res)=>{
         console.log(res);
         for(let i=0; i<res.data.length; i++){
@@ -68,30 +58,3 @@ function showUser1(){
 }
 
 showUser1();
-
-//
-//
-//
-//get request using crud crud
-// function showUser(){
-//     axios.get('https://crudcrud.com/api/373328cd2d974b7d9c8c0727f5be42c5/appointmentData')
-//     .then((res)=>{
-//         let userArray = res.data;
-//         userArray.forEach((ele) => {
-//             //console.log(ele.name);
-//             //console.log(ele.email);
-//             let userInfo = {
-//                 name: ele.name,
-//                 email: ele.email
-//             }
-//             addListOfUsers(userInfo);
-//         });
-//         console.log(res)
-//         //console.log(res.data);
-//     })
-//     .catch((err)=>{
-//         console.log(err);
-//     })
-// }
-
-// showUser();
